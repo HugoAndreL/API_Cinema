37fa02f3daa21fae67cc3645f164542d011ed0e2
Hugo André Lucena <hugo.lucena@atos.net>
2025-04-09 18:01:54 -0300

    Feature: Add a Script of Creation the test's database and the production database

---
+++ Creation Script.sql
@@ -0,0 +1,14 @@
+/* For execute only the test's database or the production database, select the code and execute */
+/* Test's DATABASE */
+USE [master];
+GO
+
+CREATE DATABASE [CinemaTestDB];
+GO
+
+/* Prodution DATABASE */
+USE [master];
+GO
+
+CREATE DATABASE [/* Your Database Name */];
+GO
